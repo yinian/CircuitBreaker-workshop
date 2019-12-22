9053c651a294ed35682bf0bbda0cd3ee03fd3c63
viagra <1356971566@qq.com>
2019-12-22 11:40:18 +0800

    Hystrix 源码部分，里面代码添加了部分注释，以及添加了log4j配置

---
+++ Hystrix/hystrix-core/src/main/java/com/netflix/hystrix/HystrixCircuitBreaker.java
@@ -0,0 +1,326 @@
+/**
+ * Copyright 2012 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.hystrix;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import com.netflix.hystrix.HystrixCommandMetrics.HealthCounts;
+import rx.Subscriber;
+import rx.Subscription;
+
+/**
+ * Circuit-breaker logic that is hooked into {@link HystrixCommand} execution and will stop allowing executions if failures have gone past the defined threshold.
+ * <p>
+ * The default (and only) implementation  will then allow a single retry after a defined sleepWindow until the execution
+ * succeeds at which point it will again close the circuit and allow executions again.
+ */
+public interface HystrixCircuitBreaker {
+
+    /**
+     * Every {@link HystrixCommand} requests asks this if it is allowed to proceed or not.  It is idempotent and does
+     * not modify any internal state, and takes into account the half-open logic which allows some requests through
+     * after the circuit has been opened
+     * 
+     * @return boolean whether a request should be permitted
+     */
+    boolean allowRequest();
+
+    /**
+     * Whether the circuit is currently open (tripped).
+     * 
+     * @return boolean state of circuit breaker
+     */
+    boolean isOpen();
+
+    /**
+     * Invoked on successful executions from {@link HystrixCommand} as part of feedback mechanism when in a half-open state.
+     */
+    void markSuccess();
+
+    /**
+     * Invoked on unsuccessful executions from {@link HystrixCommand} as part of feedback mechanism when in a half-open state.
+     */
+    void markNonSuccess();
+
+    /**
+     * Invoked at start of command execution to attempt an execution.  This is non-idempotent - it may modify internal
+     * state.
+     */
+    boolean attemptExecution();
+
+    /**
+     * @ExcludeFromJavadoc
+     * @ThreadSafe
+     */
+    class Factory {
+        // String is HystrixCommandKey.name() (we can't use HystrixCommandKey directly as we can't guarantee it implements hashcode/equals correctly)
+        private static ConcurrentHashMap<String, HystrixCircuitBreaker> circuitBreakersByCommand = new ConcurrentHashMap<String, HystrixCircuitBreaker>();
+
+        /**
+         * Get the {@link HystrixCircuitBreaker} instance for a given {@link HystrixCommandKey}.
+         * <p>
+         * This is thread-safe and ensures only 1 {@link HystrixCircuitBreaker} per {@link HystrixCommandKey}.
+         * 
+         * @param key
+         *            {@link HystrixCommandKey} of {@link HystrixCommand} instance requesting the {@link HystrixCircuitBreaker}
+         * @param group
+         *            Pass-thru to {@link HystrixCircuitBreaker}
+         * @param properties
+         *            Pass-thru to {@link HystrixCircuitBreaker}
+         * @param metrics
+         *            Pass-thru to {@link HystrixCircuitBreaker}
+         * @return {@link HystrixCircuitBreaker} for {@link HystrixCommandKey}
+         */
+        public static HystrixCircuitBreaker getInstance(HystrixCommandKey key, HystrixCommandGroupKey group, HystrixCommandProperties properties, HystrixCommandMetrics metrics) {
+            // this should find it for all but the first time
+            HystrixCircuitBreaker previouslyCached = circuitBreakersByCommand.get(key.name());
+            if (previouslyCached != null) {
+                return previouslyCached;
+            }
+
+            // if we get here this is the first time so we need to initialize
+
+            // Create and add to the map ... use putIfAbsent to atomically handle the possible race-condition of
+            // 2 threads hitting this point at the same time and let ConcurrentHashMap provide us our thread-safety
+            // If 2 threads hit here only one will get added and the other will get a non-null response instead.
+            HystrixCircuitBreaker cbForCommand = circuitBreakersByCommand.putIfAbsent(key.name(), new HystrixCircuitBreakerImpl(key, group, properties, metrics));
+            if (cbForCommand == null) {
+                // this means the putIfAbsent step just created a new one so let's retrieve and return it
+                return circuitBreakersByCommand.get(key.name());
+            } else {
+                // this means a race occurred and while attempting to 'put' another one got there before
+                // and we instead retrieved it and will now return it
+                return cbForCommand;
+            }
+        }
+
+        /**
+         * Get the {@link HystrixCircuitBreaker} instance for a given {@link HystrixCommandKey} or null if none exists.
+         * 
+         * @param key
+         *            {@link HystrixCommandKey} of {@link HystrixCommand} instance requesting the {@link HystrixCircuitBreaker}
+         * @return {@link HystrixCircuitBreaker} for {@link HystrixCommandKey}
+         */
+        public static HystrixCircuitBreaker getInstance(HystrixCommandKey key) {
+            return circuitBreakersByCommand.get(key.name());
+        }
+
+        /**
+         * Clears all circuit breakers. If new requests come in instances will be recreated.
+         */
+        /* package */static void reset() {
+            circuitBreakersByCommand.clear();
+        }
+    }
+
+
+    /**
+     * The default production implementation of {@link HystrixCircuitBreaker}.
+     * 
+     * @ExcludeFromJavadoc
+     * @ThreadSafe
+     */
+    /* package */class HystrixCircuitBreakerImpl implements HystrixCircuitBreaker {
+        private final HystrixCommandProperties properties;
+        private final HystrixCommandMetrics metrics;
+//三种状态通过枚举来定义
+        enum Status {
+            CLOSED, OPEN, HALF_OPEN;
+        }
+//三种状态通过枚举来定义
+        private final AtomicReference<Status> status = new AtomicReference<Status>(Status.CLOSED);
+//最后一次访问的时间，用于试探请求是否恢复        
+        private final AtomicLong circuitOpened = new AtomicLong(-1);
+        private final AtomicReference<Subscription> activeSubscription = new AtomicReference<Subscription>(null);
+
+        protected HystrixCircuitBreakerImpl(HystrixCommandKey key, HystrixCommandGroupKey commandGroup, final HystrixCommandProperties properties, HystrixCommandMetrics metrics) {
+            this.properties = properties;
+            this.metrics = metrics;
+
+            //On a timer, this will set the circuit between OPEN/CLOSED as command executions occur
+            Subscription s = subscribeToStream();
+            activeSubscription.set(s);
+        }
+
+        private Subscription subscribeToStream() {
+            /*
+             * This stream will recalculate the OPEN/CLOSED status on every onNext from the health stream
+             */
+            return metrics.getHealthCountsStream()
+                    .observe()
+                    .subscribe(new Subscriber<HealthCounts>() {
+                        @Override
+                        public void onCompleted() {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+
+                        }
+
+                        @Override
+                        public void onNext(HealthCounts hc) {
+                            // check if we are past the statisticalWindowVolumeThreshold
+                            if (hc.getTotalRequests() < properties.circuitBreakerRequestVolumeThreshold().get()) {
+                                // we are not past the minimum volume threshold for the stat window,
+                                // so no change to circuit status.
+                                // if it was CLOSED, it stays CLOSED
+                                // if it was half-open, we need to wait for a successful command execution
+                                // if it was open, we need to wait for sleep window to elapse
+                            } else {
+                                if (hc.getErrorPercentage() < properties.circuitBreakerErrorThresholdPercentage().get()) {
+                                    //we are not past the minimum error threshold for the stat window,
+                                    // so no change to circuit status.
+                                    // if it was CLOSED, it stays CLOSED
+                                    // if it was half-open, we need to wait for a successful command execution
+                                    // if it was open, we need to wait for sleep window to elapse
+                                } else {
+                                    // our failure rate is too high, we need to set the state to OPEN
+                                    if (status.compareAndSet(Status.CLOSED, Status.OPEN)) {
+                                        circuitOpened.set(System.currentTimeMillis());
+                                    }
+                                }
+                            }
+                        }
+                    });
+        }
+//将熔断器置于关闭状态，并重置统计数据
+        @Override
+        public void markSuccess() {
+            if (status.compareAndSet(Status.HALF_OPEN, Status.CLOSED)) {
+                //This thread wins the race to close the circuit - it resets the stream to start it over from 0
+                metrics.resetStream();
+                Subscription previousSubscription = activeSubscription.get();
+                if (previousSubscription != null) {
+                    previousSubscription.unsubscribe();
+                }
+                Subscription newSubscription = subscribeToStream();
+                activeSubscription.set(newSubscription);
+                circuitOpened.set(-1L);
+            }
+        }
+//将熔断器置于打开状态
+        @Override
+        public void markNonSuccess() {
+            if (status.compareAndSet(Status.HALF_OPEN, Status.OPEN)) {
+                //This thread wins the race to re-open the circuit - it resets the start time for the sleep window
+                circuitOpened.set(System.currentTimeMillis());
+            }
+        }
+//用于判断熔断器是否打开
+        @Override
+        public boolean isOpen() {
+            if (properties.circuitBreakerForceOpen().get()) {
+                return true;
+            }
+            if (properties.circuitBreakerForceClosed().get()) {
+                return false;
+            }
+            return circuitOpened.get() >= 0;
+        }
+//用于判断是否放行流量
+        @Override
+        public boolean allowRequest() {
+            if (properties.circuitBreakerForceOpen().get()) {
+                return false;
+            }
+            if (properties.circuitBreakerForceClosed().get()) {
+                return true;
+            }
+            //第一次请求肯定就放行了
+            if (circuitOpened.get() == -1) {
+                return true;
+            } else {//半开状态将不放行
+                if (status.get().equals(Status.HALF_OPEN)) {
+                    return false;
+                } else {
+                    return isAfterSleepWindow();
+                }
+            }
+        }
+//根据当前时间与最后一次请求的时候进行比较，当超过了设置的SleepWindowInMilliseconds，将放行请求用于试探服务访问是否OK
+        private boolean isAfterSleepWindow() {
+            final long circuitOpenTime = circuitOpened.get();
+            final long currentTime = System.currentTimeMillis();
+            final long sleepWindowTime = properties.circuitBreakerSleepWindowInMilliseconds().get();
+            return currentTime > circuitOpenTime + sleepWindowTime;
+        }
+//用于试探服务是否OK的方法
+        @Override
+        public boolean attemptExecution() {
+            if (properties.circuitBreakerForceOpen().get()) {
+                return false;
+            }
+            if (properties.circuitBreakerForceClosed().get()) {
+                return true;
+            }
+            if (circuitOpened.get() == -1) {
+                return true;
+            } else {
+                if (isAfterSleepWindow()) {
+                    //only the first request after sleep window should execute
+                    //if the executing command succeeds, the status will transition to CLOSED
+                    //if the executing command fails, the status will transition to OPEN
+                    //if the executing command gets unsubscribed, the status will transition to OPEN
+                    if (status.compareAndSet(Status.OPEN, Status.HALF_OPEN)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    /**
+     * An implementation of the circuit breaker that does nothing.
+     * 
+     * @ExcludeFromJavadoc
+     */
+    /* package */static class NoOpCircuitBreaker implements HystrixCircuitBreaker {
+
+        @Override
+        public boolean allowRequest() {
+            return true;
+        }
+
+        @Override
+        public boolean isOpen() {
+            return false;
+        }
+
+        @Override
+        public void markSuccess() {
+
+        }
+
+        @Override
+        public void markNonSuccess() {
+
+        }
+
+        @Override
+        public boolean attemptExecution() {
+            return true;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CircuitBreaker-workshop
 断路器，比如Hystrix等
+
+## Hystrix - 源码部分
+
+## hystrix-tutorial - 一些例子
+
+
